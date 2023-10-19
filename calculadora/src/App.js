4bf68393e0eb2fe0c950515af2ac8d6933cb7aef
CristianMG99 <cristian.mendoza@ias.com.co>
2023-10-19 11:58:19 -0500

    Implementation Imc Calculator

--- calculadora/src/App.js
+++ calculadora/src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
+import React,{useState} from 'react'
 import './App.css';
-
 function App() {
+  const [peso,setPeso]=useState("");
+  const [altura,setAltura]=useState("");
+  const [imc,setImc]=useState(null);
+  const [imcStatus,setImcStatus]=useState("");
+
+  const cambiarPeso=(e)=>{
+    setPeso(e.target.value)
+  }
+  const cambiarAltura=(e)=>{
+    setAltura(e.target.value)
+  }
+  const calcularIMC = ()=>{
+    if(altura&&peso){
+      if(!altura||!peso){
+        setImc("")
+        setImcStatus("")
+      }
+      
+      const alturaMetros=altura/100
+      const imc= peso/(alturaMetros*alturaMetros)
+      setImc(imc)
+
+      if (imc<18.5){
+        setImcStatus("Te encuentras en Bajo Peso")
+      }else if (imc >18.5 && imc <24.9){
+        setImcStatus("Te encuentras en Normal")
+
+      }else if (imc>25 && imc <29.9){
+        setImcStatus("Te encuentras en Sobre Peso")
+
+      }else{
+        setImcStatus("Te encuentras en Obesidad")
+
+      }
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <h1>CALULADORA</h1>
+   <h1>Ingrese su altura</h1>
+   <input type="number" value={altura} onChange={cambiarAltura}></input>
+   <h1>Ingrese su peso</h1>
+   <input type="number" value={peso}onChange={cambiarPeso}></input>
+   
+   <h1>SU IMC ES: {imc}</h1>
+   <h1> {imcStatus}</h1>
+   
+   <button onClick={calcularIMC}>CALCULAR AHORA!</button>
     </div>
   );
 }
